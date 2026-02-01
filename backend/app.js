e3f19cf462b114ea68720ee6cfd7f1a2d955a7e8
Budvin Perera <budvinperera@gmail.com>
2026-02-01 11:08:49 +0530

    setup home page

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const path = require("path");
 const mysql = require("mysql");
 const dotenv = require("dotenv");
 
-dotenv.config(path = "./.env");
+dotenv.config({path: "/.env"});
 
 const app = express ();
 
@@ -13,6 +14,11 @@ password: process.env.DATABASE_PASSWORD,
 database: process.env.DATABASE
 });
 
+const publicDirectory = path.join(__dirname, "./public");
+app.use(express.static(publicDirectory));
+
+app.set("view engine", "hbs");
+
 db.connect((error) => {
     if(error){
         console.log(error);
@@ -22,7 +28,8 @@ db.connect((error) => {
 })
 
 app.get("/", (req, res) => {
-res.send("<h1>Home Page</h1>")
+//res.send("<h1>Home Page</h1>")
+res.render("index");
 }) ;
 
 app. listen(5001, () => {
